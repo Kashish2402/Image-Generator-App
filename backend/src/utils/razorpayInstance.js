7e5b64ad53341602396883d96c68536e40c06317
Kashish2402 <Kashishgupta2402@gmail.com>
2025-04-01 16:09:53 +0530

    changes for deployment

--- backend/src/utils/razorpayInstance.js
+++ backend/src/utils/razorpayInstance.js
@@ -1,8 +1,9 @@
 import Razorpay from "razorpay";
 import dotenv from "dotenv"
-dotenv.config({path:"./.env"})
+dotenv.config({path:"./backend/.env"})
+
 
 export  const razorPayInstance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID ,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_secret: process.env.RAZORPAY_KEY_SECRET ,
 });
