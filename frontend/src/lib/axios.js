6b3ab344fab2a9c1b93fe3504a8e1eae10ac1fd3
Kashish2402 <Kashishgupta2402@gmail.com>
2025-04-01 17:08:12 +0530

    update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.VITE_BACKEND_URL || "https://imagen-1y5p.onrender.com/api/v1"; 
+const BASE_URL = "https://imagen-1y5p.onrender.com/api/v1"; 
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
