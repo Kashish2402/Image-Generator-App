a90794c3399766ed4ef699346aabcca9e7db1b7d
Kashish2402 <Kashishgupta2402@gmail.com>
2025-04-01 15:49:22 +0530

    deployment setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from "cookie-parser"
 import {errorHandler} from "./middlewares/error.middleware.js"
+import path from "path"
+
+const _dirname=path.resolve()
 
 const app=express()
 
@@ -21,6 +24,11 @@ import imageRoutes from './routes/image.routes.js'
 app.use("/api/v1/users",userRoutes)
 app.use("/api/v1/images",imageRoutes)
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
+app.get("*", (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+  });
+  
 app.use(errorHandler)
 export {app}
